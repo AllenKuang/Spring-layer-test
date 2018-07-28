353f2bb7b5104ae2119bd1308dc83e900b28cd9b
beeMonkey <675383749@qq.com>
2018-07-28 23:42:34 +0800

    write the test for getCompanyById and getAllCompany in company controller

--- src/main/java/com/springboot/employeeapiboot/one_to_more/controllers/CompanysController.java
+++ src/main/java/com/springboot/employeeapiboot/one_to_more/controllers/CompanysController.java
@@ -18,50 +18,30 @@ import java.util.List;
 @RequestMapping("/api/v1/companies")
 public class CompanysController {
     private CompanyService companyService;
-    private CompanyRepository companyRepository;
     @Autowired
-    public CompanysController(CompanyRepository companyRepository,CompanyService companyService){
-        this.companyRepository=companyRepository;
+    public CompanysController(CompanyService companyService){
         this.companyService=companyService;
     }
-//    @Autowired
-//    public CompanysController(CompanyService companyService){
-//        this.companyService=companyService;
-//    }
     @Transactional
     @GetMapping(path="",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Company>> getAllCompany(){
         return ResponseEntity.ok(companyService.getAllCompany());
-        //return ResponseEntity.ok(companyRepository.findAll());
-        //return companyRepository.findAll();
+
     }
     @Transactional
     @GetMapping(path="/{id}")
     public CompanyDTO getCompanyById(@PathVariable long id){
-            //employeeRepository.findAllById(id);
         return companyService.getCompanyById(id);
-//          Company  company  =companyRepository.findById(id).get();
-//        //return companyRepository.findById(id).get();
-//        return new CompanyDTO(company);
     }
     @Transactional
     @PostMapping(path="")
     public Company addOneCompany(@RequestBody Company company){
         return companyService.addOneCompany(company);
-//        company.getEmployees().stream().forEach(employee ->{
-//            employee.setCompany(company);
-//        });
-//        return companyRepository.save(company);
-        //return company;
     }
     @Transactional
     @PutMapping("/{id}")
     public Company modifyCompanyById(@RequestBody Company company){
         return companyService.modifyCompanyById(company);
-//        company.getEmployees().stream().filter(employee -> employee.getCompany() == null).forEach(employee -> {
-//            employee.setCompany(company);});
-//        //ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-//        return companyRepository.save(company);
     }
     @DeleteMapping("/{id}")
     public ResponseEntity deleteCompanyById(@PathVariable long id){
--- src/test/java/com/springboot/employeeapiboot/one_to_more/controllers/CompanysControllerTest.java
+++ src/test/java/com/springboot/employeeapiboot/one_to_more/controllers/CompanysControllerTest.java
@@ -1,30 +1,69 @@
 package com.springboot.employeeapiboot.one_to_more.controllers;
 
+import com.springboot.employeeapiboot.one_to_more.dto.CompanyDTO;
 import com.springboot.employeeapiboot.one_to_more.entities.Company;
+import com.springboot.employeeapiboot.one_to_more.services.CompanyService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(CompanysController.class)
 public class CompanysControllerTest {
     @Autowired
     CompanysController companysController;
-
-    //@MockBean
-
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private CompanyService companyService;
 
     @Test
-    public void should_get_all_companys_without_any_paramters() {
+    public void should_get_all_companys_without_any_paramters() throws Exception {
+        //given
+        Company company1=new Company(1L,"oocl");
+        Company company2=new Company(2L,"tw");
+        List<Company> companies= Arrays.asList(company1,company2);
+        when(companyService.getAllCompany()).thenReturn(companies);
 
+        //when
+        ResultActions result=mockMvc.perform(get("/api/v1/companies"));
+
+        //then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(2)))
+                .andExpect(jsonPath("$[0].id",is(1)))
+                .andExpect(jsonPath("$[0].name",is(company1.getName())));
     }
 
     @Test
-    public void getCompanyById() {
+    public void get_company_by_id() throws Exception {
+        //given
+        long id=1;
+        CompanyDTO companyDTO=new CompanyDTO(new Company(1L,"oocl"));
+        when(companyService.getCompanyById(anyLong())).thenReturn(companyDTO);
+        //when
+         mockMvc.perform(get("/api/v1/companies/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(id))
+                .andExpect(jsonPath("name").value(companyDTO.getName()));
     }
 
     @Test
